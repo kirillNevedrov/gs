6b0133fd06fa7eac34eb2c57af43813ba326e2b0
Kirill Nevedrov <kirill.nevedrov@gmail.com>
2016-03-02 03:15:07 +0300

    add quilt refactored to be able to manage text field state

--- wwwroot/js/components/addQuilt.jsx
+++ wwwroot/js/components/addQuilt.jsx
@@ -2,26 +2,41 @@ import React from 'react';
 import {connect} from 'react-redux';
 import RaisedButton from 'material-ui/lib/raised-button';
 import TextField from 'material-ui/lib/text-field';
-
 import {addQuilt} from 'js/actionCreators/actionCreators';
 
-let AddQuilt = ({dispatch}) => {
-    let input;
+class AddQuilt extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {textFieldValue: ''};
+    }
 
-    let onClick = () => {
-        dispatch(addQuilt(input.getValue()))
-    };
+    _handleClick() {
+        this.props.dispatch(addQuilt(this.state.textFieldValue))
+        this.setState({
+            textFieldValue: ''
+        });
+    }
 
-    return (
-        <div>
-            <TextField
-                hintText="ToDo Task"
-                ref={node => {input = node;}}
-            /><br/>
-            <RaisedButton label="Add Quilt" onClick={onClick} />
-        </div>
-    );
-};
+    _handleTextFieldChange(e) {
+        this.setState({
+            textFieldValue: e.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <TextField
+                    hintText="ToDo Task"
+                    value={this.state.textFieldValue}
+                    onChange={e => this._handleTextFieldChange(e)}
+                /><br/>
+                <RaisedButton
+                    label="Add Quilt"
+                    onClick={e => this._handleClick(e)}/>
+            </div>
+        );
+    }
+}
 
 export default connect()(AddQuilt);
-
