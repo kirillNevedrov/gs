82ad09e13bc6c11a8d52d48c602ce1e0f281b6fb
Kirill Nevedrov <kirill.nevedrov@gmail.com>
2016-03-08 00:55:35 +0300

    react motion transition

--- wwwroot/js/components/routingMotion.js
+++ wwwroot/js/components/routingMotion.js
@@ -8,69 +8,32 @@ class RoutingMotion extends React.Component {
     constructor() {
         super();
         this.state = {
-            isReset: false
         };
         this.lastKey = null;
         this.previousKey = null;
         this.lastPath = null;
         this.previousPath = null;
-        this.lastChild = null;
-        this.previousChild = null;
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            isReset: true
-        });
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if(this.state.isReset)
-        {
-            this.setState({
-                isReset: false
-            });
-        }
     }
 
     willLeave(transition) {
         if (transition.data.path === this.previousPath) {
-            //if (transition.style.x.val === 0) {
-            //    return {x: 0};
-            //}
-
-            return {x: spring(-100, {stiffness: 50, damping: 5})};
+            return {y: spring(-100, {stiffness: 50, damping: 5}), x: 0};// add isLeaving bool key here?
         }
 
         return null;
     }
 
     willEnter() {
-        return {x: 100};
+        return {x: 100, y: 0};
     }
 
     render() {
         if (this.lastPath !== this.props.location.pathname) {
             this.previousPath = this.lastPath;
             this.lastPath = this.props.location.pathname;
-            this.previousChild = this.lastChild ? React.cloneElement(this.lastChild) : null;
-            this.lastChild = this.props.children;
             this.previousKey = this.lastKey;
             this.lastKey = uuid.v1();
         }
-
-        let styles = this.state.isReset && this.previousChild
-            ? [{
-                            key: this.previousKey,
-                            style:{x: 0},
-                            data: {path: this.previousPath, children: this.previousChild}
-                            }]
-            : [{
-            key: this.lastKey,
-            style:{x: spring(0, {stiffness: 50, damping: 5})},
-            data: {path: this.lastPath, children: this.props.children}
-        }];
-
 
         return (
             <TransitionMotion
@@ -78,17 +41,22 @@ class RoutingMotion extends React.Component {
                 willLeave={this.willLeave.bind(this)}
                 defaultStyles={[{
                             key: this.lastKey,
-                            style:{x: 100},
+                            style:{x: 100, y: 0},
                             data: {path: this.lastPath, children: this.props.children}
                             }]}
-                styles={styles}
+                styles={[{
+            key: this.lastKey,
+            style:{x: spring(0, {stiffness: 50, damping: 5}), y: 0},
+            data: {path: this.lastPath, children: this.props.children}
+        }]}
             >
                 {interpolatedStyles => {
 
 
                     return (<div>
                         {interpolatedStyles.map(config => {
-                            let style = {transform: `translateX(${config.style.x}%)`};
+                            let isLeaving = config.data.path !== this.lastPath
+                            let style = {transform: `translateX(${isLeaving ? config.style.y: config.style.x}%)`};
 
                             let children = React.cloneElement(config.data.children, {
                                 key: config.key,
